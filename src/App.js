a20534ad1055c87c39832e6536de6cb5e23b39c6
jamshid <jamshidazizov@gmail.com>
2019-12-12 12:31:55 +0200

    initial personalization

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>
+          DELYS
+        </h3>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ens0.herokuapp.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          by enso
         </a>
       </header>
     </div>
