8675693d466b7d0952ac3693ea99cf97648fe2d3
jamshid <jamshidazizov@gmail.com>
2019-12-12 13:52:09 +0200

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Particles from 'react-particles-js';
 const particlesOptions = {
   particles: {
     "number": {
-      "value": 25,
+      "value": 30,
       "density": {
         "enable": true,
         "value_area": 800
