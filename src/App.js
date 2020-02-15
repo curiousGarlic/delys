41488bccb2e11862ca5cf19e67674103ae821eec
jamshid <jamshidazizov@gmail.com>
2020-02-15 12:32:19 +0200

    adding shop pafge, collection-preview and collection-item components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 import soycandles from './assets/soycandles.jpg';
 import gelcandles from './assets/gelcandles.jpg';
 
@@ -24,9 +25,11 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>DELYS</h1>
+                <h3>ДОМ ПОДАРКОВ</h3>
+                <h1>DE LYS</h1>
                 <Switch>
                     <Route exact path='/' component={ HomePage }/>
+                    <Route exact path='/shop' component={ ShopPage } /> 
                     <Route exact path='/soycandles' component={ SoyCandles } />
                     <Route exact path='/gelcandles' component={ GelCandles }/>
                 </Switch>
